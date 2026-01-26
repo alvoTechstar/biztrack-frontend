677ba0492e578a22bac87f391d5be037365f3c6c
alvin.saiguran <alvin.saiguran@interswitchgroup.com>
2026-01-26 14:31:54 +0300

    jsx fix

--- src/views/sign-in/ForgotPassword/OTPRequest.jsx
+++ src/views/sign-in/ForgotPassword/OTPRequest.jsx
@@ -3,7 +3,7 @@ import KeyRoundedIcon from "@mui/icons-material/KeyRounded";
 import "./forgotPassword.css";
 import TitleHeader from "../../../components/header/TitleHeader";
 import TextInput from "../../../components/Input/TextInput";
-import { validateEmail } from "../../../utilities/Sharedfunctions";
+import { validateEmail } from "../../../utilities/Sharedfunctions.jsx";
 import FormButton from "../../../components/buttons/FormButton";
 import TextButton from "../../../components/buttons/TextButton";
 
--- src/views/sign-in/Login.jsx
+++ src/views/sign-in/Login.jsx
@@ -4,7 +4,7 @@ import PasswordInput from "../../components/Input/PasswordInput"
 import FormButton from "../../components/buttons/FormButton";
 import NaviButton from "../../components/buttons/Navibutton";
 import loginBg from "../../assets/Backgrounds/background.png";
-import { validateEmail, validatePassword } from "../../utilities/Sharedfunctions"; 
+import { validateEmail, validatePassword } from "../../utilities/Sharedfunctions.jsx"; 
 import { useDispatch } from "react-redux";
 import { authActions } from "../../store";
 import ModalFooter from "../../components/footer/ModalFooter";
